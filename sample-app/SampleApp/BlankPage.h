ca9873f9aab3b6689a9e5ab735a8f50e1fb7eb27
Roxk <vzox.rock@gmail.com>
2023-04-05 23:49:44 +0800

    Add IdlGenInclude

---
+++ sample-app/SampleApp/BlankPage.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "pch.h"
+#include "BlankPage.g.h"
+
+namespace winrt::SampleApp::implementation
+{
+    struct BlankPage : BlankPageT<BlankPage>
+    {
+        BlankPage() 
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::SampleApp::factory_implementation
+{
+    struct BlankPage : BlankPageT<BlankPage, implementation::BlankPage>
+    {
+    };
+}
