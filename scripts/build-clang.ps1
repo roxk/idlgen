4cfb915d1999f1a5785c62ebe67c6cea9968740c
Roxk <vzox.rock@gmail.com>
2023-04-08 23:00:15 +0800

    Fix CI

--- scripts/build-clang.ps1
+++ scripts/build-clang.ps1
@@ -12,6 +12,4 @@ if (!(test-path $ninjaBuildDir)) {
 	echo "build-ninja doesn't 'exist. Run populdate-build-ninja.ps1 first"
 	exit 0
 }
-cd $ninjaBuildDir
-cmd /c "vcvars64.bat && ninja"
-cd ..
+cmd /c "cd `"$ninjaBuildDir`" && vcvars64.bat && ninja"
--- scripts/download-clang-lib.ps1
+++ scripts/download-clang-lib.ps1
@@ -6,7 +6,7 @@ $outRootDir = "$srcDir\..\build-ninja\Release"
 $zipFile = "$outRootDir\lib.zip"
 if (!(test-path $zipFile)) {
 	$ProgressPreference = 'SilentlyContinue'
-	$url = "https://www.dropbox.com/s/p7tk5xdrtjnr744/clang-lib-release-win-x64.zip?dl=1"
+	$url = "https://www.dropbox.com/s/mxb2hez53j21vf7/clang-lib-release-win-x64.zip?dl=1"
 	Invoke-WebRequest $url -OutFile $zipFile
 }
 Expand-Archive $zipFile -DestinationPath $outRootDir -force
