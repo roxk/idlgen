63ab5d60e661efa791f2037bc5cf7c5d2c9c6fa0
Roxk <vzox.rock@gmail.com>
2023-04-11 23:22:24 +0800

    Add extension project #3

---
+++ extension/Idlgen.Cpp/Idlgen.Cpp/Commands/CppGenerateIDL.cs
@@ -0,0 +1,11 @@
+﻿namespace Idlgen.Cpp
+{
+    [Command(PackageIds.CppGenerateIDL)]
+    internal sealed class CppGenerateIDL : BaseCommand<CppGenerateIDL>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("Idlgen.Cpp", "Button clicked");
+        }
+    }
+}
