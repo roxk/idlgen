2800649ea36fae42f71f0e0f7fa018f28836e917
Roxk <vzox.rock@gmail.com>
2023-05-25 21:59:03 +0800

    ci-cd: Use idlgen clang lib #52

--- scripts/download-clang-lib.ps1
+++ scripts/download-clang-lib.ps1
@@ -4,7 +4,7 @@ $outRootDir = "$srcDir\..\build-ninja\Release"
 $zipFile = "$outRootDir\lib.zip"
 if (!(test-path $zipFile)) {
 	$ProgressPreference = 'SilentlyContinue'
-	$url = "https://www.dropbox.com/s/mxb2hez53j21vf7/clang-lib-release-win-x64.zip?dl=1"
+	$url = "https://www.dropbox.com/s/4wf0851vwpvj2p7/clang-idlgen-lib-release-win-64.zip?dl=1"
 	Invoke-WebRequest $url -OutFile $zipFile
 }
 Expand-Archive $zipFile -DestinationPath $outRootDir -force
