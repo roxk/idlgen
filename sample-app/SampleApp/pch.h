2a22d5f20468153cfd0630ed8536e0596cc1af1d
Roxk <vzox.rock@gmail.com>
2023-05-30 22:41:30 +0800

    Update pch.h #55

--- sample-app/SampleApp/pch.h
+++ sample-app/SampleApp/pch.h
@@ -6,6 +6,8 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 #include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.UI.h>
+#include <winrt/Windows.UI.Composition.h>
 #include <winrt/Windows.UI.Xaml.h>
 #include <winrt/Windows.UI.Xaml.Controls.h>
 #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
