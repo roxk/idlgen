466f46c1d9344dd0dc4e6efeb3799ab8f219eeb1
Roxk <vzox.rock@gmail.com>
2023-05-30 22:28:28 +0800

    Handle .xaml.g.h's using #55

--- test-data/include/OutdatedProjectionXaml.xaml.g.h
+++ test-data/include/OutdatedProjectionXaml.xaml.g.h
@@ -5,6 +5,8 @@ namespace winrt::Root::implementation
 	template <typename D, typename... I>
 	struct OutdatedProjectionXamlT : OutdatedProjectionXaml_base<D, I...>
 	{
-
+		using base_type = typename OutdatedProjectionXamlT::base_type;
+		using base_type::base_type;
+		using class_type = typename OutdatedProjectionXamlT::class_type;
 	};
 }
