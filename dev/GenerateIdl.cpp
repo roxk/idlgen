78957ac22d1891c0ba2f37f0fcb114c757cb12eb
Roxk <vzox.rock@gmail.com>
2023-06-05 11:33:33 +0800

    Make idlgen generate fake projection #70

--- dev/GenerateIdl.cpp
+++ dev/GenerateIdl.cpp
@@ -32,6 +32,7 @@ namespace lc = llvm::cl;
 namespace ct = clang::tooling;
 namespace lfs = llvm::sys::fs;
 namespace lsp = llvm::sys::path;
+namespace stdfs = std::filesystem;
 
 namespace idlgen
 {
@@ -83,11 +84,17 @@ static lc::opt<bool> Help("h", lc::desc("Alias for -help"), lc::Hidden);
 
 static lc::opt<bool> Generate("gen", lc::desc("Generate IDL next to the input file"));
 
+static lc::opt<bool> GenerateFakeProjection(
+    "gen-fake-projection", lc::desc("Generate fake projection. --generated-files-dir and --root-namespace is required.")
+);
+
 static lc::opt<std::string> GenerateOutputPath(
     "gen-out", lc::desc("If specified and --gen is applied, control the output path of the generated IDL")
 );
 
 static lc::opt<std::string> RootNamespace("root-namespace", lc::desc("Root namespace"));
+
+static lc::opt<std::string> GeneratedFilesDir("--generated-files-dir", lc::desc("Path of Generated Files"));
 
 static lc::list<std::string> Includes("include", lc::desc("Include folder(s)"));
 
@@ -118,81 +125,99 @@ static void PrintVersion(llvm::raw_ostream& OS)
     OS << "idlgen 0.0.1" << '\n';
 }
 
-void TrimIncludeStatementForFileName(std::string& includeStatement)
-{
-    if (auto lastQuoteIndex = includeStatement.rfind("\""))
-    {
-        includeStatement.erase(lastQuoteIndex, 1);
-    }
-    if (auto firstQuoteIndex = includeStatement.find("\""))
-    {
-        includeStatement.erase(0, firstQuoteIndex + 1);
-    }
-}
-
-clang::FileID GetFileId(clang::SourceManager& sourceManager, std::string_view filePath)
-{
-    auto& fileManager{sourceManager.getFileManager()};
-    clang::FileEntry const* entry{};
-    if (auto fileEntry = fileManager.getFile(filePath))
-    {
-        entry = *fileEntry;
-    }
-    return sourceManager.getOrCreateFileID(entry, clang::SrcMgr::C_User);
-}
-
-llvm::StringRef GetCode(clang::SourceManager& sourceManager, std::string_view filePath)
-{
-    return sourceManager.getBufferData(GetFileId(sourceManager, filePath));
-}
-
-std::string StripImplementationProjectionFromHeader(
-    bool verbose, std::vector<std::string> const& clangArgs, clang::StringRef file, clang::StringRef buffer
+bool GenerateFakeProjectionFromHeader(
+    clang::StringRef buffer
 )
 {
     const std::string code{buffer.str()};
-    clang::LangOptions defaultLangOpt;
     constexpr auto regexStr = "^#\\s*include\\s*\"\\w*\\.g\\.h\"";
     std::regex includeRegex(regexStr);
     auto includeDirectiveMatch{std::sregex_iterator(code.begin(), code.end(), includeRegex)};
     if (includeDirectiveMatch == std::sregex_iterator())
     {
-        return buffer.data();
+        return true;
     }
     auto firstMatchPosition{(*includeDirectiveMatch)[0].first - code.begin()};
-    auto newCode{std::regex_replace(code, includeRegex, "")};
-    if (newCode.size() == code.size())
+    // Find namespace
+    constexpr auto namespaceStr = "namespace\\s+(\\w|::|)+\\s*\\{";
+    std::regex namespaceRegex(namespaceStr);
+    std::smatch namespaceResults;
+    constexpr auto namespaceIndex = 0;
+    if (!std::regex_search(code.begin(), code.end(), namespaceResults, namespaceRegex))
     {
-        return buffer.data();
+        return true;
+    }
+    // "Canonicalize" to WinRT namespace, i.e. remove implementation or factory_implementation
+    // Using :_: as this is an invalid C++ token
+    auto namespaceMatchResult{namespaceResults[namespaceIndex]};
+    std::string namespaceDefinition{
+        std::regex_replace(namespaceMatchResult.str(), std::regex("::implementation"), ":_:")};
+    if (namespaceDefinition.size() == namespaceMatchResult.length())
+    {
+        namespaceDefinition = std::regex_replace(namespaceDefinition, std::regex("::factory_implementation"), ":_:");
     }
     // Find struct Class : ClassT<Class>
     constexpr auto baseRegexStr =
         "(struct|class)(\\s|\\w|\\[|\\]|:|\"|\\(|\\)|,|\\.|\\\\)+(\\w+)\\s+:(\\s+\\w+,*)*(\\s+\\3T)<\\3>";
     std::regex baseRegex(baseRegexStr);
-    constexpr auto captureGroupCount = 5;
-    constexpr auto expectedMatchCount = captureGroupCount + 1;
+    constexpr auto baseCaptureGroupCount = 5;
+    constexpr auto expectedBaseMatchCount = baseCaptureGroupCount + 1;
     constexpr auto classNameIndex = 3;
-    std::string projectionReplacement{"#include <winrt/"};
-    projectionReplacement += RootNamespace;
-    projectionReplacement += ".h>\n";
+    std::vector<std::string> classNames;
     for (auto it = code.begin(); it != code.end();)
     {
         std::smatch results;
-        if (!std::regex_search(it, code.end(), results, baseRegex) || results.size() < expectedMatchCount)
+        if (!std::regex_search(it, code.end(), results, baseRegex) || results.size() < expectedBaseMatchCount)
         {
             break;
         }
         it = results[0].second;
-        projectionReplacement += "template <typename T, typename... I> struct ";
-        projectionReplacement += results[classNameIndex];
-        projectionReplacement += "T {};\n";
+        classNames.emplace_back(results[classNameIndex]);
     }
-    if (projectionReplacement.empty())
+    if (classNames.empty())
     {
         return buffer.data();
     }
-    newCode.insert(firstMatchPosition, projectionReplacement);
-    return newCode;
+    for (auto&& name : classNames)
+    {
+        std::string projectionReplacement{"#pragma once\n\n"};
+        projectionReplacement += "#include <winrt/";
+        projectionReplacement += RootNamespace;
+        projectionReplacement += ".h>\n";
+        projectionReplacement += std::regex_replace(namespaceDefinition, std::regex(":_:"), "");
+        projectionReplacement += "\n";
+        projectionReplacement += "struct ";
+        projectionReplacement += name;
+        projectionReplacement += " {};\n";
+        projectionReplacement += "}\n"; // namespace $RootNamespace
+        projectionReplacement += std::regex_replace(namespaceDefinition, std::regex(":_:"), "::implementation");
+        projectionReplacement += "\n";
+        projectionReplacement += "template <typename T, typename... I> struct ";
+        projectionReplacement += name;
+        projectionReplacement += "T {};\n";
+        projectionReplacement += "}\n"; // namespace implementation
+        projectionReplacement += std::regex_replace(namespaceDefinition, std::regex(":_:"), "::factory_implementation");
+        projectionReplacement += "\n";
+        projectionReplacement += "template <typename T, typename... I> struct ";
+        projectionReplacement += name;
+        projectionReplacement += "T {};\n";
+        projectionReplacement += "}\n"; // namespace factory_implementation
+        std::cout << projectionReplacement << std::endl;
+        std::error_code ec;
+        stdfs::path outPath{GeneratedFilesDir.getValue()};
+        outPath.append("");
+        llvm::raw_fd_ostream out(
+            outPath.string(), ec, lfs::CreationDisposition::CD_CreateAlways, lfs::FileAccess::FA_Write, lfs::OpenFlags::OF_None
+        );
+        if (ec)
+        {
+            std::cerr << "Failed to open .idlgen.h output" << std::endl;
+            std::cerr << ec.message() << std::endl;
+            return false;
+        }
+        out << projectionReplacement;
+    }
+    return true;
 }
 
 /// <summary>
@@ -244,10 +269,18 @@ int main(int argc, const char** argv)
         std::cerr << "No files specified" << std::endl;
         return 1;
     }
-    if (!GeneratePch && RootNamespace.empty())
+    if (GenerateFakeProjection)
     {
-        std::cerr << "Root namespace must be provided when not generating PCH" << std::endl;
-        return 1;
+        if (RootNamespace.empty())
+        {
+            std::cerr << "Root namespace must be provided when generating fake projection" << std::endl;
+            return 1;
+        }
+        else if (GeneratedFilesDir.empty())
+        {
+            std::cerr << "Generated Files must be provided when generating fake projection" << std::endl;
+            return 1;
+        }
     }
     std::vector<std::string> clangArgs{
         "-xc++-header",
@@ -279,6 +312,30 @@ int main(int argc, const char** argv)
                   << std::endl;
         return 1;
     }
+    if (GenerateFakeProjection)
+    {
+        for (auto&& filePath : FileNames)
+        {
+            auto codeOrErr{llvm::MemoryBuffer::getFileAsStream(filePath)};
+            if (std::error_code ec = codeOrErr.getError())
+            {
+                std::cerr << ec.message() << std::endl;
+                return 1;
+            }
+            auto code(std::move(codeOrErr.get()));
+            if (code->getBufferSize() == 0)
+            {
+                continue;
+            }
+            const auto result{GenerateFakeProjectionFromHeader(code->getBuffer())};
+            if (!result)
+            {
+                std::cerr << "fatal: Failed to generate fake projection for " << filePath << std::endl;
+                return 1;
+            }
+        }
+        return 0;
+    }
     std::vector<std::string> getterTemplates(GetterTemplates);
     std::vector<std::string> propertyTemplates(PropertyTemplates);
     std::optional<std::reference_wrapper<std::string>> outputFile;
@@ -299,7 +356,7 @@ int main(int argc, const char** argv)
             auto ec{lfs::create_directories(PchOutDir.c_str())};
             if (ec)
             {
-                std::cerr << "Failed to create pch output dir" << std::endl;
+                std::cerr << "fatal: Failed to create pch output dir" << std::endl;
                 return 1;
             }
         }
@@ -314,7 +371,7 @@ int main(int argc, const char** argv)
         auto code(std::move(codeOrErr.get()));
         if (code->getBufferSize() == 0)
         {
-            std::cerr << "Failed to get pch buffer" << std::endl;
+            std::cerr << "fatal: Failed to get pch buffer" << std::endl;
             return 1;
         }
         auto buffer{code->getBuffer()};
@@ -399,7 +456,7 @@ int main(int argc, const char** argv)
         {
             return 1;
         }
-        auto buffer{StripImplementationProjectionFromHeader(Verbose, clangArgs, filePath, code->getBuffer())};
+        auto buffer{code->getBuffer()};
         const auto result = ct::runToolOnCodeWithArgs(
             std::make_unique<idlgen::GenIdlFrontendAction>(
                 out.value().get(), Verbose, getterTemplates, propertyTemplates
