6c7286b1c477b6eaaf2aa4975f2e58028869e368
Roxk <vzox.rock@gmail.com>
2023-06-07 21:58:58 +0800

    Add sample app cli test #69
    Fix not adding xaml provider idl

---
+++ scripts/test-sample-app.ps1
@@ -0,0 +1,5 @@
+$scriptDir = $PSScriptRoot
+$sampleAppDir = "$scriptDir\..\sample-app"
+$buildCmd = "msbuild $sampleAppDir -t:clean -t:Restore -t:build -p:Configuration=Debug -p:Platform=x64 | out-host"
+powershell -c "Import-Module `"`"`"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll`"`"`";`
+Enter-VsDevShell d5206e10 -SkipAutomaticLocation -DevCmdArguments `"`"`"-arch=x64 -host_arch=x64`"`"`";$buildCmd"
