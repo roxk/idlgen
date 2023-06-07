450df22c244bf773f9f4a1143967ec22f1fec8ad
Roxk <vzox.rock@gmail.com>
2023-06-07 22:16:45 +0800

    Run sample app test in ci #69

--- scripts/test-sample-app.ps1
+++ scripts/test-sample-app.ps1
@@ -1,5 +1,7 @@
+$ErrorActionPreference = "Stop"
 $scriptDir = $PSScriptRoot
 $sampleAppDir = "$scriptDir\..\sample-app"
-$buildCmd = "msbuild $sampleAppDir -t:clean -t:Restore -t:build -p:Configuration=Debug -p:Platform=x64 | out-host"
+nuget restore $sampleAppDir
+$buildCmd = "msbuild $sampleAppDir -t:clean -t:build -p:Configuration=Debug -p:Platform=x64 | out-host"
 powershell -c "Import-Module `"`"`"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll`"`"`";`
 Enter-VsDevShell d5206e10 -SkipAutomaticLocation -DevCmdArguments `"`"`"-arch=x64 -host_arch=x64`"`"`";$buildCmd"
