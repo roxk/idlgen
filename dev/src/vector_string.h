4ba07f204d9fb435c86aabaf39681c28a5a0d6f5
Roxk <vzox.rock@gmail.com>
2026-05-19 00:07:52 +0800

    style: fix format #102

--- dev/src/vector_string.h
+++ dev/src/vector_string.h
@@ -1,10 +1,12 @@
 class vector_string
 {
-public:
+  public:
     constexpr vector_string() = default;
-    constexpr vector_string(std::string_view str) {
+    constexpr vector_string(std::string_view str)
+    {
         _data.resize(str.size());
-        for (size_t i = 0; i < str.size(); ++i) {
+        for (size_t i = 0; i < str.size(); ++i)
+        {
             _data[i] = str[i];
         }
     }
@@ -12,37 +14,47 @@ public:
     constexpr vector_string(vector_string&& that) = default;
     constexpr vector_string& operator=(const vector_string& that) = default;
     constexpr vector_string& operator=(vector_string&& that) = default;
-    constexpr vector_string& operator+=(std::string const& str) {
+    constexpr vector_string& operator+=(std::string const& str)
+    {
         *this += std::string_view(str);
         return *this;
     }
-    constexpr vector_string& operator+=(const char* str) {
+    constexpr vector_string& operator+=(const char* str)
+    {
         *this += std::string_view(str);
         return *this;
     }
-    constexpr vector_string& operator+=(const vector_string& that) {
+    constexpr vector_string& operator+=(const vector_string& that)
+    {
         _data.reserve(_data.size() + that._data.size());
-        for (auto c : that._data) {
+        for (auto c : that._data)
+        {
             _data.push_back(c);
         }
         return *this;
     }
-    constexpr vector_string& operator+=(std::string_view str) {
+    constexpr vector_string& operator+=(std::string_view str)
+    {
         _data.reserve(_data.size() + str.size());
-        for (auto c : str) {
+        for (auto c : str)
+        {
             _data.push_back(c);
         }
         return *this;
     }
-    constexpr void reserve(size_t size) {
+    constexpr void reserve(size_t size)
+    {
         _data.reserve(size);
     }
-    constexpr const std::vector<char>& data() const {
+    constexpr const std::vector<char>& data() const
+    {
         return _data;
     }
-    constexpr const size_t size() const {
+    constexpr const size_t size() const
+    {
         return _data.size();
     }
+
   private:
     std::vector<char> _data;
 };
