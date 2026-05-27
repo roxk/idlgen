ee64216b9eb0409e39d07ccc2be783eaca8f7a04
Roxk <vzox.rock@gmail.com>
2026-05-28 01:06:53 +0800

    fix: fix not supporting including implementation header in pch #137

---
+++ sample-app/SampleApp/OldSchoolImplementationType.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "OldSchoolImplementationType.g.h"
+
+namespace winrt::SampleApp::implementation
+{
+    struct OldSchoolImplementationType : OldSchoolImplementationTypeT<OldSchoolImplementationType>
+    {
+        OldSchoolImplementationType() = default;
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+    };
+}
+
+namespace winrt::SampleApp::factory_implementation
+{
+    struct OldSchoolImplementationType : OldSchoolImplementationTypeT<OldSchoolImplementationType, implementation::OldSchoolImplementationType>
+    {
+    };
+}
